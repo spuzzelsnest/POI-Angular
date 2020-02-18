49092688d6c7aa61559dcfd4cced1da1ec811c9f
spuzzelsnest <j.mpdesmet@gmail.com>
2020-02-18 23:19:09 +0100

    json not loading

--- src/clt/app/_services/marker.service.ts
+++ src/clt/app/_services/marker.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import * as L from 'leaflet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkerService {
-  articles: string = '/assets/data/markers.json';
 
-  constructor(private http: HttpClient) {}
+  articles: string = 'assets/data/markers.json';
+    
+  constructor(private http: HttpClient) { }
     
   makeArticleMarkers(map: L.map): void {
       this.http.get(this.articles).subscribe((res: any) => {
+        
          for (const c of res.features) {
-            const lat = c[0];
-            
-            const marker = L.marker([0, 0]).addTo(map)
+          //  const lat = c.lat[0];
+        //  const lon = c.long[1];
+        //    const marker = L.marker([lat, lon]).addTo(map);
+      console.log(c);
       }
     });
   }
--- src/clt/app/map/map.component.ts
+++ src/clt/app/map/map.component.ts
@@ -4,6 +4,21 @@ import { icon, Marker } from 'leaflet';
 
 import { MarkerService } from '../_services/marker.service';
 
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
