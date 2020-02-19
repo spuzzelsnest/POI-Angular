3742cccfb44685c39e9c2fa812d3560a560a87fd
spuzzelsnest <j.mpdesmet@gmail.com>
2020-02-19 22:00:03 +0100

    no db final

--- src/clt/app/_services/marker.service.ts
+++ src/clt/app/_services/marker.service.ts
@@ -7,20 +7,11 @@ import * as L from 'leaflet';
   providedIn: 'root'
 })
 export class MarkerService {
-
-  articles: string = 'assets/data/markers.json';
     
   constructor(private http: HttpClient) { }
     
   makeArticleMarkers(map: L.map): void {
-      this.http.get(this.articles).subscribe((res: any) => {
-        
-         for (const c of res.features) {
-          //  const lat = c.lat[0];
-        //  const lon = c.long[1];
-        //    const marker = L.marker([lat, lon]).addTo(map);
-      console.log(c);
-      }
-    });
+
+      const circle = L.circleMarker([50.1, 6]).addTo(map);
   }
 }
