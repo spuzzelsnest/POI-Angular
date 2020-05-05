6124f9cad26e6dbd27527ca151b76719ab102c5c
spuzzelsnest <j.mpdesmet@gmail.com>
2020-05-05 23:56:39 +0200

    markers loading prep

--- src/clt/app/_services/marker.service.ts
+++ src/clt/app/_services/marker.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import * as pics from '../../assets/data/markers.json'
 import * as L from 'leaflet';
 
 @Injectable({
@@ -11,7 +11,19 @@ export class MarkerService {
   constructor(private http: HttpClient) { }
     
   makeArticleMarkers(map: L.map): void {
+      console.log("Json data : ", JSON.stringify(pics));
+    this.http.get(this.pics).subscribe((res: any) => {
+      for (const p of res.features) {
+        
+        const lat = p.lat;
+        const lng = p.lng;
+        const marker = L.marker([lon, lng]).addTo(map);
+      }
+    
+    });
+        const marker1 = L.marker([49, 6]).addTo(map);
+      //const circle = L.circleMarker([50.1, 6]).addTo(map);
 
-      const circle = L.circleMarker([50.1, 6]).addTo(map);
+
   }
 }
