b350d52716d7a1a713978d1fa8260c3614019c77
spuzzelsnest <j.mpdesmet@gmail.com>
2020-05-08 00:12:05 +0200

    json markers working

--- src/clt/app/_services/marker.service.ts
+++ src/clt/app/_services/marker.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as pics from '../../assets/data/markers.json'
+import pics from '../../assets/data/markers.json';
 import * as L from 'leaflet';
 
 @Injectable({
@@ -11,19 +11,14 @@ export class MarkerService {
   constructor(private http: HttpClient) { }
     
   makeArticleMarkers(map: L.map): void {
-      console.log("Json data : ", JSON.stringify(pics));
-    this.http.get(this.pics).subscribe((res: any) => {
-      for (const p of res.features) {
-        
-        const lat = p.lat;
-        const lng = p.lng;
-        const marker = L.marker([lon, lng]).addTo(map);
-      }
-    
+      
+    pics.forEach(function (value) {
+  console.log(value.lat)
+        const lat = value.lat;
+        const lng = value.lng;
+        const marker = L.marker([lat, lng]).addTo(map);
     });
-        const marker1 = L.marker([49, 6]).addTo(map);
-      //const circle = L.circleMarker([50.1, 6]).addTo(map);
-
-
+    //const marker1 = L.marker([49, 6]).addTo(map);
+    //const circle = L.circleMarker([50.1, 6]).addTo(map);
   }
 }
