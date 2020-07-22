d224f8ba468bb79af2e21ac3b572da08f38735df
spuzzelsnest <j.mpdesmet@gmail.com>
2020-07-22 23:34:27 +0200

    added env apiUrl

--- src/clt/app/map/legenda/legenda.component.ts
+++ src/clt/app/map/legenda/legenda.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { MediaSelect } from '../../media-select.service';
-
 @Component({
   selector: 'app-legenda',
   templateUrl: './legenda.component.html',
