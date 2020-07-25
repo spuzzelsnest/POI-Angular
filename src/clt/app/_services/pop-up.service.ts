8e7fc89078bcaa25ddf6f8400cb2b1cfd5126532
spuzzelsnest <j.mpdesmet@gmail.com>
2020-07-25 20:01:53 +0200

    set style for the popup

--- src/clt/app/_services/pop-up.service.ts
+++ src/clt/app/_services/pop-up.service.ts
@@ -10,9 +10,11 @@ export class PopUpService {
   makePicPopup(pic: any): string {
       
     const style = `<style>`+
-          `.leaflet-popup-tip{display: none !important;}`+
-          `.leaflet-popup, .leaflet-zoom-animated, .leaflet-popup-content{ width:600px; height:400px;}`+
+          `.leaflet-popup-tip, .leaflet-popup-tip-container{display: none !important;}`+
+          `.leaflet-popup{display:block !important;}`+
+          `.leaflet-popup, .leaflet-zoom-animated, .leaflet-popup-content{ width:600px;height:500px;}`+
           `.leaflet-popup-content{ width:auto !important;padding:5px;border:1px solid grey;}`+
+          `#featureInfo{overflow: auto;}`+
           `#title, #photo, #video{text-align:center;}`+
           `</style>`;
       
@@ -20,15 +22,17 @@ export class PopUpService {
     if (pic.typeId < 3){ 
         return `` +
           style +
-          `<div id="title">Place: ${pic.place} (${pic.country}) - ${pic.date }</div>` +
-          `<div id="photo"><img src="/assets/pictures/${pic.name}.jpg"></div>`+
-          `<div id="info">${pic.info}</div>`
+          `<div id="featureInfo"`+
+          `<div id="title">${pic.title}</div>` +
+          `<div id="photo"><img src="/assets/pictures/${pic.name}.jpg"><br>Place: ${pic.place} (${pic.country}) - ${pic.date }</div>`+
+          `<div id="info"><hr><p>${pic.info}</p></div></div>`
     }else{
         return `` +
           style +
-          `<div id="title">Place: ${pic.place} (${pic.country}) - ${pic.date }</div>` +
-          `<div id="video"><video  poster="/assets/media/${pic.name}/${pic.name}.jpg" width="480" height="360" controls="autoplay"><source src="/assets/media/${pic.name}/${pic.name}.mp4" type="video/mp4"><source src="/assets/media/${pic.name}/${pic.name}.ogg" type="video/ogg"></div>`+
-          `<div id="info">${pic.info}</div>`
+          `<div id="featureInfo"`+
+          `<div id="title">${pic.title}</div>` +
+          `<div id="video"><video poster="/assets/media/${pic.name}/${pic.name}.jpg" width="480" height="360" controls="autoplay"><source src="/assets/media/${pic.name}/${pic.name}.mp4" type="video/mp4"><source src="/assets/media/${pic.name}/${pic.name}.ogg" type="video/ogg"></video><br>Place: ${pic.place} (${pic.country}) - ${pic.date}</div>`+
+          `<div id="info"><hr><p>${pic.info}</p></div></div>`
     }
   }
 }
