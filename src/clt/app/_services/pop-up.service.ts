647c558668fdc65c3f6a863e13dfee16f1de0e82
spuzzelsnest <j.mpdesmet@gmail.com>
2020-07-25 18:29:35 +0200

    changes in popoup display

--- src/clt/app/_services/pop-up.service.ts
+++ src/clt/app/_services/pop-up.service.ts
@@ -8,17 +8,27 @@ export class PopUpService {
   constructor() { }
 
   makePicPopup(pic: any): string {
+      
+    const style = `<style>`+
+          `.leaflet-popup-tip{display: none !important;}`+
+          `.leaflet-popup, .leaflet-zoom-animated, .leaflet-popup-content{ width:600px; height:400px;}`+
+          `.leaflet-popup-content{ width:auto !important;padding:5px;border:1px solid grey;}`+
+          `#title, #photo, #video{text-align:center;}`+
+          `</style>`;
+      
     
     if (pic.typeId < 3){ 
         return `` +
-          `<div><h1><u>Place: ${pic.place} (${pic.country}) - ${pic.date }</h1></u></div>` +
-          `<div><img src="/assets/pictures/${pic.name}.jpg"> </div>`+
-          `<br><div>${pic.info} </div>`
+          style +
+          `<div id="title">Place: ${pic.place} (${pic.country}) - ${pic.date }</div>` +
+          `<div id="photo"><img src="/assets/pictures/${pic.name}.jpg"></div>`+
+          `<div id="info">${pic.info}</div>`
     }else{
-          return `` +
-          `<div>Place: ${pic.place} (${pic.country}) - ${pic.date }</div><br>` +
-          `<p><center><video  poster="/assets/media/${pic.name}/${pic.name}.jpg" width="480" height="360" controls="autoplay"><source src="/assets/media/${pic.name}/${pic.name}.mp4" type="video/mp4"><source src="/assets/media/${pic.name}/${pic.name}.ogg" type="video/ogg"></center>`+
-          `<br><div>${pic.info} </div>`
+        return `` +
+          style +
+          `<div id="title">Place: ${pic.place} (${pic.country}) - ${pic.date }</div>` +
+          `<div id="video"><video  poster="/assets/media/${pic.name}/${pic.name}.jpg" width="480" height="360" controls="autoplay"><source src="/assets/media/${pic.name}/${pic.name}.mp4" type="video/mp4"><source src="/assets/media/${pic.name}/${pic.name}.ogg" type="video/ogg"></div>`+
+          `<div id="info">${pic.info}</div>`
     }
   }
 }
